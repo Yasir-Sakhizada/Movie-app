6d3f7cc5ff927a958e12721eb96e7d09124c8b8d
A.Yasir <yasirsakhizada072@gmail.com>
2025-12-26 20:27:10 +0430

    another one

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Routes, Route,  HashRouter} from "react-router-dom";
+import { Routes, Route,BrowserRouter} from "react-router-dom";
 import "./index.css";
 import TrailerPage from "./components/TrailerPage.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/trailer/:id" element={<TrailerPage />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Movie-app",
+  base:"/",
   plugins: [react(), eslint()],
 });
